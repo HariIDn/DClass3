5e670149c9ed1510964387db699e45fca9fff298
HariIDn <aldifaadit75@gmail.com>
2022-10-03 22:15:47 +0700

    Read

--- DClass3/Program.cs
+++ DClass3/Program.cs
@@ -6,18 +6,36 @@ using System.Threading.Tasks;
 
 namespace DClass3
 {
-    class bubleShort
+    class Program
     {
-        public void display()
-        {
+        private int[] a = new int[20];
 
-        }
-        public void cara()
-        {
+        private int n;
 
+        public void read()
+        {
+            while (true)
+            {
+                Console.Write("Masukan Banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Masukan Elemen Array");
+            Console.WriteLine("--------------------");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
         }
-    internal class Program
-    {
         static void Main(string[] args)
         {
         }
